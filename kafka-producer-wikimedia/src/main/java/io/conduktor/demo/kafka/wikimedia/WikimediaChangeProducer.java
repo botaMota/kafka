526bf4a040cf5d449af514ea41ab7bbe3037afd6
botamota <boutahir.elmoutaoukil@gmail.com>
2023-08-11 21:28:20 +0100

    Kafka wikimedia change producer

--- kafka-producer-wikimedia/src/main/java/io/conduktor/demo/kafka/wikimedia/WikimediaChangeProducer.java
+++ kafka-producer-wikimedia/src/main/java/io/conduktor/demo/kafka/wikimedia/WikimediaChangeProducer.java
@@ -2,9 +2,12 @@ package io.conduktor.demo.kafka.wikimedia;
 
 import com.launchdarkly.eventsource.EventSource;
 import com.launchdarkly.eventsource.EventSource.Builder;
+import com.launchdarkly.eventsource.MessageEvent;
+import com.launchdarkly.eventsource.StreamException;
 import com.launchdarkly.eventsource.background.BackgroundEventHandler;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -28,6 +31,11 @@ public class WikimediaChangeProducer {
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
+        //set high throughput producer configs
+        properties.setProperty(ProducerConfig.COMPRESSION_TYPE_CONFIG, "snappy");
+        properties.setProperty(ProducerConfig.LINGER_MS_CONFIG, "20");
+        properties.setProperty(ProducerConfig.BATCH_SIZE_CONFIG, Integer.toString(32*1024));
+
         //create producer
         KafkaProducer<String,String> producer = new KafkaProducer(properties);
 
@@ -37,6 +45,23 @@ public class WikimediaChangeProducer {
         String url = "https://stream.wikimedia.org/v2/stream/recentchange";
         Builder builder = new Builder(URI.create(url));
         EventSource eventSource = builder.build();
+
+        try {
+            eventSource.start();
+            Iterable<MessageEvent> messageEvents = eventSource.messages();
+
+            messageEvents.forEach(msg ->{
+                log.info("details msg : "+msg.getLastEventId() + " | "+msg.getEventName());
+                ProducerRecord<String,String> producerRecord = new ProducerRecord(topic ,msg.getData());
+                producer.send(producerRecord);
+            });
+
+            producer.close();
+        } catch (StreamException e) {
+            throw new RuntimeException(e);
+        }
+
+
 
         //start the producer in another thread
         //eventSource.start();
